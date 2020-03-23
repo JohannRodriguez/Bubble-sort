33953cb853a8c6b5f562ea39f09c78d8e8a3046a
JohannRodriguez <54152734+JohannRodriguez@users.noreply.github.com>
2020-03-23 08:38:57 -0700

    Bubble sort changed to work with yield

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -2,7 +2,7 @@ def bubble_sort_by(array)
   loop do
     exitLoop = false
     (array.length - 1).times do |i|
-      if (array[i].length - array[i + 1].length) > 0
+      if yield(array[i] - array[i + 1]) > 0
         array[i], array[i + 1] = array[i + 1], array[i]
         exitLoop = true
       end
@@ -11,5 +11,5 @@ def bubble_sort_by(array)
   end
 end
 
-print bubble_sort_by(["hi","hello","hey"])
-print bubble_sort_by(["perro","sol","murciélago","gato","esfera"])
+print bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length }
+print bubble_sort_by(["perro","sol","murciélago","gato","esfera"]) { |left, right| left.length - right.length }
