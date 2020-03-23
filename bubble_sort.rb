de529f758f0c95149e16bc419cf55b020196e3bf
JohannRodriguez <54152734+JohannRodriguez@users.noreply.github.com>
2020-03-23 08:31:07 -0700

    Bubble sort input for user string added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -11,6 +11,10 @@ def bubble_sort(array)
   end
 end
 
+puts "Add multiple random numberes by writting them separated with a coma without spaces, like: 1,2,3"
+user_array = gets.chomp.split(",").map { |i| i.to_i}
+
+print bubble_sort(user_array)
 print bubble_sort([1,4,1,3,2,5,7,5,3])
 print bubble_sort([20,40,75,80,10,30,25])
 print bubble_sort([131,256,846,996,314,211])
